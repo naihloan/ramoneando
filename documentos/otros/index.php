bdeedeac96eaa9258afb9143771001c1e0626194
nilon <benjij_1980@yahoo.com>
2016-07-26 16:40:19 -0300

    many commits

--- documentos/otros/index.php
+++
@@ -1,5 +0,0 @@
- <?php
-include("templates/header.htm");
-include("templates/index.htm");
-include("templates/footer.htm"); 
-
--- documentos/otros/params.js
+++
@@ -1,6 +0,0 @@
-{
-  "name": "Ramoneando",
-  "tagline": "",
-  "body": "# ramoneando\r\n============\r\n\r\nHow-To Clone a Repository (go to the proper folder):\r\ngit clone https://github.com/naihloan/ramoneando\r\n\r\n\r\nAdd, commit, and push your changes:\r\n\r\ngit add --all\r\n\r\ngit commit -m \"Initial commit\"\r\n\r\ngit push -u origin master\r\n\r\n\r\n\r\n\r\nUsing Node.js\r\n=============\r\n\r\nYou'll need:\r\n\r\nTo run this app, save it to a file called app.js, npm install koa and run % \"node app &\" from your command line. Next, visit http://localhost:3000. In your browser.\r\n",
-  "note": "Don't delete this file! It's used internally to help with page regeneration."
-}
